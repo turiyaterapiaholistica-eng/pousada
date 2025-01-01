6a6d05f020e67f6d7af31815feaf555298b5eb7a
thiagovilha <vilha1986@gmail.com>
2025-01-01 14:15:51 -0300

    GestaoComandas_Login

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+print("Current directory:", os.getcwd())
+print("Python path:", sys.path)
 
 application = get_wsgi_application()
