82e86a6a9001b9556663f3e6fc12f0c9e3e8f236
thiagovilha <vilha1986@gmail.com>
2025-01-06 20:18:56 -0300

    New_Try_After_Login

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -158,6 +158,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://pousada-zbcs.onrender.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
