6300918335031147afc98f1c431ceea0233338d6
thiagovilha <vilha1986@gmail.com>
2025-01-06 21:06:24 -0300

    New_Try_After_Login

--- backend/cozinha/migrations/0011_create_superuser.py
+++ backend/cozinha/migrations/0011_create_superuser.py
@@ -9,11 +9,15 @@ def create_superuser(apps, schema_editor):
         password='vilhaadmin'
     )
 
+def reverse_func(apps, schema_editor):
+    User = apps.get_model("auth", "User")
+    User.objects.get(username="admin").delete()
+
 class Migration(migrations.Migration):
     dependencies = [
         ('cozinha', '0011_create_superuser'),  # Update this
     ]
 
     operations = [
-        migrations.RunPython(create_superuser),
+        migrations.RunPython(create_superuser, reverse_func),
     ]
