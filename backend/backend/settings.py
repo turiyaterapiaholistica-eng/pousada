a33224b5b489449268e311aaee3af9760892c256
thiagovilha <vilha1986@gmail.com>
2025-03-26 10:14:19 -0300

    create_superuser

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,16 +149,16 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
-# Auto-create superuser if missing
-if os.getenv('CREATE_SUPERUSER', 'False').lower() == 'true':
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(is_superuser=True).exists():
-        User.objects.create_superuser(
-            username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
-            email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
-            password=os.getenv('DJANGO_SUPERUSER_PASSWORD'),
-        )
+# # Auto-create superuser if missing
+# if os.getenv('CREATE_SUPERUSER', 'False').lower() == 'true':
+#     from django.contrib.auth import get_user_model
+#     User = get_user_model()
+#     if not User.objects.filter(is_superuser=True).exists():
+#         User.objects.create_superuser(
+#             username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
+#             email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
+#             password=os.getenv('DJANGO_SUPERUSER_PASSWORD'),
+#         )
 
 
 # Default primary key field type
